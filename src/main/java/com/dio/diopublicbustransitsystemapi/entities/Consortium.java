80271d39b55874c94901fc63b36f4ee1b1b51247
FellipeToledo <fellipetoledo.dev@gmail.com>
2021-09-07 14:37:56 -0300

    consortium entity built

---
+++ src/main/java/com/dio/diopublicbustransitsystemapi/entities/Consortium.java
@@ -0,0 +1,30 @@
+package com.dio.diopublicbustransitsystemapi.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Consortium {
+
+      @Id
+      @Column
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long id;
+
+      @Column
+      private String name;
+
+      @Column
+      private String orderCode;
+
+}
