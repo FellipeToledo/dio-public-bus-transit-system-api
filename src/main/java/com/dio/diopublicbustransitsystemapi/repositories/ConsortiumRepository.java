b7aad4da20a5826146c2c74d678bc8d4acbba78e
FellipeToledo <fellipetoledo.dev@gmail.com>
2021-09-07 15:29:49 -0300

    ConsortiumRepository interface built

---
+++ src/main/java/com/dio/diopublicbustransitsystemapi/repositories/ConsortiumRepository.java
@@ -0,0 +1,9 @@
+package com.dio.diopublicbustransitsystemapi.repositories;
+
+import com.dio.diopublicbustransitsystemapi.entities.Consortium;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsortiumRepository extends JpaRepository<Consortium, Long> {
+
+}
