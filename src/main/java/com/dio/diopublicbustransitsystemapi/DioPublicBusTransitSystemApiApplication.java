3f5b1c86a7dd4de804e1be9f26399608aa445ef5
FellipeToledo <fellipetoledo.dev@gmail.com>
2021-09-07 14:06:09 -0300

    first commit

---
+++ src/main/java/com/dio/diopublicbustransitsystemapi/DioPublicBusTransitSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.diopublicbustransitsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioPublicBusTransitSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioPublicBusTransitSystemApiApplication.class, args);
+	}
+
+}
